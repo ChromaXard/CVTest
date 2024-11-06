f4e906f8c901f597e90009bf3b0327c33b1cf27e
ChromaXard <axel.kastler@outlook.fr>
2024-11-06 15:44:06 +0100

    setting up hash router for multiple pages on site

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -1,17 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-      <Router>
       <div>
         <Navbar />
         <Routes>
@@ -20,7 +20,6 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
-    </Router>
       </header>
       <main className="App-main"></main>
       <footer className="App-footer"></footer>
